0de401247b2005dbdc07d73c7ab96e857d6deef2
AlexanderMac <amatsibarov@gmail.com>
2018-04-23 23:08:53 +0300

    remove jQuery dependency

--- src/app/vendor.js
+++ src/app/vendor.js
@@ -1,9 +1,7 @@
-import 'bootstrap/dist/js/bootstrap';
+import 'angular';
+import 'angular-route';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'lodash';
 import 'spinkit/css/spinkit.css';
 import 'toastr';
 import 'toastr/build/toastr.css';
-
-import 'angular';
-import 'angular-route';
