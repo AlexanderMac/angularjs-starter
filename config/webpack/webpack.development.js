8b1dbe652ee31c7bcbaf75a23e1b07e2d2229fce
AlexanderMac <amatsibarov@gmail.com>
2018-04-29 12:17:58 +0300

    fix webpack configs, update gitignore, add missing index.pug

--- config/webpack/webpack.development.js
+++ config/webpack/webpack.development.js
@@ -5,13 +5,7 @@ const commonConfig = require('./webpack.common.js');
 const helpers      = require('./helpers');
 
 module.exports = webpackMerge(commonConfig, {
-  // devtool: 'eval',
-  // devtool: 'source-map',
-  // devtool: 'hidden-source-map',
   devtool: 'inline-source-map',
-  // devtool: 'eval-source-map',
-  // devtool: 'cheap-source-map',
-  // devtool: 'cheap-module-source-map',
 
   devServer: {
     contentBase: helpers.root('dist'),
--- config/webpack/webpack.production.js
+++ config/webpack/webpack.production.js
@@ -5,13 +5,7 @@ const webpackMerge = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 
 module.exports = webpackMerge(commonConfig, {
-  // devtool: 'eval',
   devtool: 'source-map',
-  // devtool: 'hidden-source-map',
-  // devtool: 'inline-source-map',
-  // devtool: 'eval-source-map',
-  // devtool: 'cheap-source-map',
-  // devtool: 'cheap-module-source-map',
 
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
