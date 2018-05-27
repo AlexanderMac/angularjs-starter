dce627ce2a1d261010abe12a2eddffed64e19e65
AlexanderMac <amatsibarov@gmail.com>
2018-05-28 00:42:03 +0300

    use LocalStorageRepo service in User service

--- src/app/users/service.js
+++ src/app/users/service.js
@@ -1,33 +1,34 @@
 export class UserService {
-  constructor(MemoryRepoService) {
-    this.memoryRepoSrvc = MemoryRepoService;
+  constructor(LocalStorageRepoService) {
+    this.repoSrvc = LocalStorageRepoService;
+    this.repoSrvc.init('Users');
   }
 
   getUser(id) {
-    return this.memoryRepoSrvc
+    return this.repoSrvc
       .getOne(id)
       .then(res => res.data);
   }
 
   getUsers() {
-    return this.memoryRepoSrvc
+    return this.repoSrvc
       .getList()
       .then(res => res.data);
   }
 
   createUser(user) {
-    return this.memoryRepoSrvc
+    return this.repoSrvc
       .create(user)
       .then(res => res.data);
   }
 
   updateUser(userData) {
-    return this.memoryRepoSrvc
+    return this.repoSrvc
       .update(userData)
       .then(res => res.data);
   }
 
   deleteUser(id) {
-    return this.memoryRepoSrvc.delete(id);
+    return this.repoSrvc.delete(id);
   }
 }
